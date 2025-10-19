c0aac461b43521f94a882b792ab8840e432bf88e
lucasmdv <lucasmdv11@gmail.com>
2025-10-18 22:42:39 -0300

    footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EndpointTest } from "./pages/endpoint-test/endpoint-test";
+import { Footer } from './components/footer/footer';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EndpointTest],
+  imports: [RouterOutlet,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
