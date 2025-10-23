1430842fa5530dbd3780c647f69aac0a03688806
Tomas cabrera <tomascabrera918@gmail.com>
2025-10-23 17:56:45 -0300

    Add category cards display to endpoint test page
    Introduces category card rendering and grid layout to the endpoint test page. Imports CategoryCard, fetches categories via CategoryService, and updates HTML/CSS to show categories in a grid for debugging purposes.

--- src/app/pages/endpoint-test/endpoint-test.ts
+++ src/app/pages/endpoint-test/endpoint-test.ts
@@ -3,27 +3,44 @@ import { ProductService } from '../../services/product-service';
 import { Product } from '../../models/product';
 import Swal from 'sweetalert2';
 import { ProductCard } from "../../components/product-card/product-card";
+import { Category } from '../../models/category';
+import { CategoryService } from '../../services/category-service';
+import { CategoryCard } from "../../components/category-card/category-card";
 
 
 @Component({
   selector: 'app-endpoint-test',
-  imports: [ProductCard],
+  imports: [ProductCard, CategoryCard],
   templateUrl: './endpoint-test.html',
   styleUrl: './endpoint-test.css'
 })
 export class EndpointTest implements OnInit {
   products: Product[] = [];
+  categories: Category[] = [];
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService, private categoryService:CategoryService) {}
 
   ngOnInit(): void {
     this.renderProducts();
+    this.renderCategories();
   }
 
   renderProducts(): void {
     this.productService.getList().subscribe({
       next: (products) => {
         this.products = products;
+        
+      },
+      error: (err) => {
+        console.error('Error al obtener todos los productos:', err);
+      }
+    });
+  }
+
+    renderCategories(): void {
+    this.categoryService.getList().subscribe({
+      next: (data ) => {
+        this.categories = data;
         
       },
       error: (err) => {
