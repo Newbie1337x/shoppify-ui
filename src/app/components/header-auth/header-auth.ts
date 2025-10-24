e8fa6fdfc571e6dbd24ba81868998a805626d988
joacoloool <joaquinalbornoz555@gmail.com>
2025-10-23 22:21:25 -0300

    header-auth created

---
+++ src/app/components/header-auth/header-auth.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-auth',
+  imports: [],
+  templateUrl: './header-auth.html',
+  styleUrl: './header-auth.css'
+})
+export class HeaderAuth {
+
+}
