277b83de66c5dae3f2cf854d119f3319abde9140
lucasmdv <lucasmdv11@gmail.com>
2025-10-29 21:48:06 -0300

    guards implementation and creation of HasRoleDirective

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { SearchResults } from './pages/search-results/search-results';
-import { EditProductPage } from './pages/edit-product-page/edit-product-page';
 import { Login } from './pages/login/login';
 import { Main } from './layouts/main/main';
 import { Auth } from './layouts/auth/auth';
@@ -9,6 +8,8 @@ import { Home } from './pages/home/home';
 import { Profile } from './pages/profile/profile';
 import { CartPage } from './pages/cart-page/cart-page';
 import { ProductsPage } from './pages/products-page/products-page';
+import { authGuard } from './core/guards/auth-guard';
+import { publicGuard } from './core/guards/public-guard';
 
 
 export const routes: Routes = [
@@ -17,8 +18,8 @@ export const routes: Routes = [
     path: 'auth',
     component: Auth,
     children: [
-      { path: 'login', component: Login },
-      { path: 'register', component: Register },
+      { path: 'login', component: Login, canActivate: [publicGuard] },
+      { path: 'register', component: Register, canActivate: [publicGuard] },
       { path: '', redirectTo: 'login', pathMatch: 'full' },
       { path: '**', redirectTo: 'login' }
     ]
@@ -33,8 +34,8 @@ export const routes: Routes = [
       { path: 'categories/:id', component: SearchResults },
       { path: 'products', component: ProductsPage},
       { path: 'products/:id', component: SearchResults },
-      { path: 'profile', component: Profile },
-      { path: 'cart', component: CartPage},
+      { path: 'profile', component: Profile, canActivate: [authGuard] },
+      { path: 'cart', component: CartPage, canActivate: [authGuard] },
       { path: '**', redirectTo: 'home', pathMatch: 'full' }
   
 
