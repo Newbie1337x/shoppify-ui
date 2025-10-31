fabd42210b66dcb53c5a5db289d163c6c14ffbbe
rivatoDev <tomasrivara23.7@gmail.com>
2025-10-30 22:04:57 -0300

    reutilize product-page implementation

--- src/app/components/product-table/product-table.ts
+++ src/app/components/product-table/product-table.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, output } from '@angular/core';
+import { Component, input, OnInit, output } from '@angular/core';
 import { Product } from '../../models/product';
 import { Category } from '../../models/category';
 import { ProductService } from '../../services/product-service';
@@ -6,67 +6,30 @@ import { CategoryService } from '../../services/category-service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProductParams } from '../../models/filters/productParams';
 import { SwalService } from '../../services/swal-service';
+import { TableDirective } from "@coreui/angular";
 
 @Component({
   selector: 'app-product-table',
-  imports: [],
+  imports: [TableDirective],
   templateUrl: './product-table.html',
   styleUrl: './product-table.css'
 })
-export class ProductTable implements OnInit{
-  products: Product[] = []
-  refinedProducts: Product[] = []
-  output = output<Product[]>()
-  params!: ProductParams
+export class ProductTable{
+  products = input<Product[]>()
+  deleteOutput = output<void>()
 
   constructor(
     private productService: ProductService,
-    private categoryService: CategoryService,
     private swal: SwalService,
     private route: ActivatedRoute,
-    private router: Router) {}
-
-  ngOnInit(): void {
-    this.route.queryParams.subscribe(params => {
-      if (Object.keys(params)) {
-        this.params = params
-        this.renderProducts()
-        this.renderRefinedProducts(params);
-      } else {
-        this.renderProducts();
-      }
-    });
-  }
-
-  renderProducts(): void {
-    this.productService.getList().subscribe({
-      next: (products) => {
-        this.products = products;
-        this.output.emit(this.products)
-      },
-      error: () => {
-        this.swal.error("Ocurrio un error al buscar los productos")
-      }
-    });
-  }
-
-  renderRefinedProducts(filters: ProductParams) {
-    this.productService.getList(filters).subscribe({
-      next: (data) => {
-        this.refinedProducts = data;
-        this.output.emit(this.refinedProducts)
-      },
-      error: (err) => {
-        this.swal.error("Ocurrio un error al filtrar los productos")
-      }
-    });
-  }
+    private router: Router
+  ) {}
 
   deleteProduct(id: number): void {
     this.productService.delete(id).subscribe({
       next: () => {
         this.swal.success("Producto eliminado con exito!")
-        this.renderRefinedProducts(this.params);
+        this.deleteOutput.emit()
       },
       error: (err) => {
         this.swal.error("Ocurrio un error al eliminar el producto")
--- src/app/pages/products-page/products-page.ts
+++ src/app/pages/products-page/products-page.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
 import { Product } from '../../models/product';
 import { Category } from '../../models/category';
 import { ProductService } from '../../services/product-service';
 import { CategoryService } from '../../services/category-service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ProductCard } from '../../components/product-card/product-card';
 import { ProductForm } from '../../components/product-form/product-form';
 import { ProductsRefiner } from '../../components/products-refiner/products-refiner';
 import { ProductParams } from '../../models/filters/productParams';
 import { ProductTable } from '../../components/product-table/product-table';
+import { SwalService } from '../../services/swal-service';
 
 @Component({
   selector: 'app-products-page',
@@ -21,13 +21,70 @@ import { ProductTable } from '../../components/product-table/product-table';
   templateUrl: './products-page.html',
   styleUrl: './products-page.css'
 })
-export class ProductsPage {
-  products: Product[] = [];
-  categories: Category[] = [];
+export class ProductsPage implements OnInit {
+  products: Product[] = []
+  refinedProducts: Product[] = []
+  categories: Category[] = []
+  params!: ProductParams
 
-  constructor(private router: Router) {}
+  constructor(
+    private productService: ProductService,
+    private categoryService: CategoryService,
+    private swal: SwalService,
+    private route: ActivatedRoute,
+    private router: Router) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      if (Object.keys(params)) {
+        this.params = params
+        this.renderProducts()
+        this.renderRefinedProducts(params);
+      } else {
+        this.renderProducts();
+      }
+      this.renderCategories()
+    });
+  }
   
-  renderPage() {
-    this.router.navigate(['/products'])
+  renderProducts(): void {
+    this.productService.getList().subscribe({
+      next: (products) => {
+        this.products = products;
+      },
+      error: () => {
+        this.swal.error("Ocurrio un error al buscar los productos")
+      }
+    });
+  }
+
+  renderRefinedProducts(filters: ProductParams) {
+    this.productService.getList(filters).subscribe({
+      next: (data) => {
+        this.refinedProducts = data;
+      },
+      error: (err) => {
+        this.swal.error("Ocurrio un error al filtrar los productos")
+      }
+    });
+  }
+
+  renderCategories(): void {
+    this.categoryService.getList().subscribe({
+      next: (data) => {
+        this.categories = data;
+      },
+      error: (err) => {
+        console.error('Error al obtener todos los productos:', err);
+      }
+    });
+  }
+
+  onDelete() {
+    if(this.params) {
+      this.renderRefinedProducts(this.params)
+    } else {
+      this.renderProducts()
+    }
   }
 }
